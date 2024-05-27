2385576ef88888f6d0a038aa4be7e4581b7dee40
Masaki Murooka <m-murooka@aist.go.jp>
2024-05-27 15:05:16 +0900

    Add tests.

---
+++ tests/src/TestBvhConverter.cpp
@@ -0,0 +1,30 @@
+/* Author: Masaki Murooka */
+
+#include <MocapRosUtils/BvhConverter.h>
+
+#include <ros/package.h>
+
+#include <fstream>
+#include <gtest/gtest.h>
+
+using namespace MocapRosUtils;
+
+TEST(TestBvhConverter, Test1)
+{
+  // Instantiate BvhConverter and load BVH file
+  std::string bvh_filename = ros::package::getPath("mocap_ros_utils") + "/data/sample_walk.bvh";
+  BvhConverter::Configuration bvh_converter_config;
+  bvh_converter_config.print_level = 2;
+  BvhConverter bvh_converter(bvh_filename, bvh_converter_config);
+  EXPECT_EQ(156, bvh_converter.joint_traj_data_.names_.size());
+
+  // Dump URDF file
+  std::string urdf_str = bvh_converter.convertToUrdf("TestBvhConverter");
+  EXPECT_TRUE(urdf_str.size() > 0);
+}
+
+int main(int argc, char ** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
